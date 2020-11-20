37c11ca5e66d13e9916856fdcf3c7cc680621431
Luis Chiang <lhc4@njit.edu.com>
2020-11-20 12:38:06 -0400

    show profile on icon click

--- scripts/NavBar.jsx
+++ scripts/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Settings from "./Settings";
 
+import {ProfileOverlay} from "./ProfilePage/ProfileOverlay"
+import {User} from "./User";
 export default function NavBar() {
 
   const [toggleSettings, setToggleSettings] = React.useState(false);
+  const [showProfile, setShowProfile] = React.useState(false);
 
   function toggleView() {
     if (toggleSettings) {
@@ -13,11 +16,18 @@ export default function NavBar() {
       setToggleSettings(true);
     }
   }
+
+  function onProfileClick()
+  {
+    setShowProfile((show) => !show);
+  }
+
   return (
     <div className="njit-header">
       <h4 className="njit-banner">NJIT</h4>
       <h1 className="app-name">Let&apos;s Meet</h1>
-      <i className="fas fa-user-circle user-profile-button"></i>
+      <i className="fas fa-user-circle user-profile-button" onClick={onProfileClick}></i>
+      { showProfile && <ProfileOverlay onClose={onProfileClick} user={User.current} /> }
       <button onClick={toggleView} className="settings-toggle-button">Settings</button>
       { toggleSettings ? <Settings /> : null}
     </div>
