d7d8f04946e19c3f4fd09c00f472aad6b6f87d1c
jun93lee <cl678@njit.edu>
2020-11-21 20:46:20 +0000

    Add MeetingRoom

--- scripts/Home.jsx
+++ scripts/Home.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Login from './Login';
 import { Content } from './Content';
+import { Room } from './Room';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ export function Home() {
                 <Switch>
                     <Route path="/" exact component={Login} />
                     <Route path="/home" component={Content} />
+                    <Route path="/room" component={Room} />
                 </Switch>
             </div>
         </Router>
