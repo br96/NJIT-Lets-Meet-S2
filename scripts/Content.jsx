9aa1c9bd23f57e8df2698361108f76afe9597845
Luis Chiang <lhc4@njit.edu.com>
2020-11-24 02:08:50 -0400

    fix chat room and map buttons

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -9,6 +9,7 @@ import FilterForm from './Filter';
 import Notification from './Notification';
 import GoogleMapsContainer from "./GoogleMapsContainer";
 
+import {User} from './User';
 export function Content() {
   if(User.current === null) return <Redirect to="/" />
 
@@ -17,20 +18,22 @@ export function Content() {
       <NavBar />
       
       <a href="/room">
-      <button type="button" className="ChatRoom" onClick="/room">Chat Room</button>
+      <button type="button" className="ChatRoom">Chat Room</button>
       </a>
       
       <a href="/map">
-      <button type="button" className="ChatRoom" onClick="/map">Map</button>
+      <button type="button" className="ChatRoom">Map</button>
       </a>
       
       <FilterForm />
-      <div className="currentUsers">
-        <CurrentUsersContainer />
-      </div>
-      <div className="event-content-container">
-        <EventHistory />
-        <EventForm />
+      <div className="content-view">
+        <div className="currentUsers">
+          <CurrentUsersContainer />
+        </div>
+        <div className="event-content-container">
+          <EventHistory />
+          <EventForm />
+        </div>
       </div>
     </div>
   );
