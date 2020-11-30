92295a9a956690c781cb0dda483530176ac797aa
Luis Chiang <lhc4@njit.edu.com>
2020-11-30 03:24:07 -0400

    remove interest from list on remove click

--- scripts/UpdateInterestsUI.jsx
+++ scripts/UpdateInterestsUI.jsx
@@ -5,10 +5,24 @@ export function UpdateInterestsUI({email})
 {
     const [interests, setInterests] = React.useState([]);
 
+    function removeTextField(event)
+    {
+        let id = event.target.id;
+        // a button will have an id btn-interest-<num>
+        // this get the <num>
+        let num = id.substring(id.lastIndexOf('-') + 1, id.length);
+        num = parseInt(num);
+        setInterests((fields) => fields.splice(num, 1));
+    }
+
     function createTextField(text, i)
     {
-        let id = "interest"+i;
-        return <li key={id}><input type="text" id={id} defaultValue={text} /></li>
+        let id = "interest-"+i;
+        return(
+        <li key={id}>
+            <input type="text" id={id} defaultValue={text} />
+            <button onClick={removeTextField} id={"btn-"+id}>Remove</button>
+        </li>);
     }
 
     function updateInterests(data)
@@ -43,7 +57,7 @@ export function UpdateInterestsUI({email})
         let msg = [];
         for(let i = 0; i < count; i++)
         {
-            let textField = document.getElementById("interest"+i);
+            let textField = document.getElementById("interest-"+i);
             if(textField.value.length > 0) msg.push(textField.value);
         }
         msg = msg.join();
