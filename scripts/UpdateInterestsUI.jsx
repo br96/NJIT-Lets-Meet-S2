6a4c9ff2cdfc7c4226267df7063a8eb0367b6777
Luis Chiang <lhc4@njit.edu.com>
2020-11-30 03:04:29 -0400

    add text field on add click

--- scripts/UpdateInterestsUI.jsx
+++ scripts/UpdateInterestsUI.jsx
@@ -5,12 +5,18 @@ export function UpdateInterestsUI({email})
 {
     const [interests, setInterests] = React.useState([]);
 
+    function createTextField(text, i)
+    {
+        let id = "interest"+i;
+        return <li key={id}><input type="text" id={id} defaultValue={text} /></li>
+    }
+
     function updateInterests(data)
     {
         let interestsList = data.interests;
         setInterests(() => {
             return interestsList.map((entry, i) => {
-                return <li key={i}>{entry}</li>
+                return createTextField(entry, i);
             });
         });
     }
@@ -31,11 +37,40 @@ export function UpdateInterestsUI({email})
 
     getInterests();
 
+    function submitChanges()
+    {
+        let count = interests.length;
+        let msg = [];
+        for(let i = 0; i < count; i++)
+        {
+            let textField = document.getElementById("interest"+i);
+            if(textField.value.length > 0) msg.push(textField.value);
+        }
+        msg = msg.join();
+        console.log(msg);
+        Socket.emit("update interests", {
+            email: email,
+            interests: msg,
+        });
+    }
+
+    function addField()
+    {
+        let count = interests.length;
+        let list = [
+            ...interests, 
+            createTextField("", count)
+        ];
+        setInterests(() => list);
+    }
+
     return (
     <div>
         <ul>
             {interests}
         </ul>
+        <button onClick={addField}>Add</button>
+        <button onClick={submitChanges}>Submit</button>
     </div>
     );
 }
