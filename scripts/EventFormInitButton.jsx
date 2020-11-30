f4af31c0acdec6b5b842368c9a0bb6c93f79cd40
brian <aeoliun@gmail.com>
2020-11-30 17:42:16 -0500

    fixed bug where oauth name disappeared on event-form reopen and added more styling

--- scripts/EventFormInitButton.jsx
+++ scripts/EventFormInitButton.jsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import EventForm from "./EventForm";
+import { Socket } from './Socket';
 
 export default function EventFormInitButton() {
 
     const [clicked, setClicked] = React.useState(false);
+
+    const [oauthName, setOauthName] = React.useState("");
 
     function isClicked() {
         if (clicked) {
@@ -13,10 +16,17 @@ export default function EventFormInitButton() {
             setClicked(true);
         }
     }
+
+    React.useEffect(() => {
+        Socket.on(Socket.id, (data) => {
+            setOauthName(data.name);
+        });
+    });
+
     return (
         <div className='event-form-init'>
             <div type="button" className="test" onClick={isClicked}><img className="add-button" src="./static/img/add-button.png" alt=""/></div>
-            {clicked ? <EventForm /> : null}
+            {clicked ? <EventForm oauthName={oauthName}/> : null}
         </div>
     )
 }
