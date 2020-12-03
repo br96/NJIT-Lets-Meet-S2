2d095694eb4b746d48ea2746f175246928c2f41a
jun93lee <cl678@njit.edu>
2020-12-03 21:27:12 +0000

    Chat Main Page

--- scripts/Room.jsx
+++ scripts/Room.jsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
 import Notification from './Notification';
+import Inbox from './Invitation/Inbox';
 import './App.css';
 
 export function Room() {
     
     return(
-        <div className="content-room">
-            <p className="Roompage">Welcome to the Private Page</p>
+
+        <div className="chat-container">
+            <header className="chat-header"> 
+            <h1 className="Roompage">Welcome to the Chat Page</h1>
             <Notification />
+            </header>
+        <div className="content-room">
+            
+        </div>
+        <main class = "chat-main">
+            <div className = "chat-sidebar">
+                <h3 className="room_name"> Room Name: </h3>
+                <h2 id="room_id"> </h2>
+                <h3 className="room_users"> Users: </h3>
+                <ul id="user_id"> </ul>
+            </div>
+            <div class="chat-messages">
+                <a href="/">
+                <button type="button" className="leave">Leave Room</button>
+                </a>
+                
+                <Inbox />
+            </div>
+        </main>
         </div>
         );
 }
