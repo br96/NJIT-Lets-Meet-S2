4f5cbd4ffa3a1e2ef300d4b8f92c751d2779352f
brian <aeoliun@gmail.com>
2020-12-03 17:58:26 -0500

    heroku testing again

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ load_dotenv(dotenv_path)
 sql_user = os.environ['SQL_USER']
 sql_pwd = os.environ['SQL_PASSWORD']
 
-database_uri = "postgresql://{}:{}@localhost/postgres".format(sql_user,sql_pwd) # use this for local testing
+# database_uri = "postgresql://{}:{}@localhost/postgres".format(sql_user,sql_pwd) # use this for local testing
 
 database_uri = os.getenv("DATABASE_URL") # use this for heroku launch
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
