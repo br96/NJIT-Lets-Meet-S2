1ef3dee71d85f5c78e2b0b7ca4d43f8c68399925
Luis Chiang <lhc4@njit.edu.com>
2020-12-07 02:09:02 -0400

    dont display friend req prompt if already a friend

--- scripts/CurrentUsersContainer.jsx
+++ scripts/CurrentUsersContainer.jsx
@@ -6,7 +6,8 @@ export default function CurrentUsersContainer() {
     const [currentUsers, setCurrentUsers] = React.useState([]);
     const [currentConnectionStatus, setCurrentConnectionStatus] = React.useState([]);
     const [currentUserEmails, setCurrentUserEmails] = React.useState([]);
-    const [friends, setFriends] = React.useState([]);
+    const [friendComponents, setFriendComponents] = React.useState([]);
+    const [friends, setFriends] = React.useState(new Set());
 
     function updateUsersHistory(data) {
         console.log(data["all_current_user_emails"]);
@@ -28,7 +29,14 @@ export default function CurrentUsersContainer() {
     function updateFriends(data)
     {
         let allFriends = data.friends;
-        setFriends(() => {
+        
+        setFriends( () => {
+            return new Set(allFriends.map((friend) => {
+                return friend.email;
+            }));
+        });
+
+        setFriendComponents(() => {
             return allFriends.map((friend, index) => {
                 return <CurrentUser
                     key={index}
@@ -48,25 +56,32 @@ export default function CurrentUsersContainer() {
     }
     getFriends();
 
+    console.log(friends);
+
     return (
         <div className="all-users-container">
             <div className="all-friends-container">
                 <div className="current-user-container-header">Friends</div>
                 <div className="all-friends-div">
-                    {friends}
+                    {friendComponents}
                 </div>
             </div>
             <div className="all-current-users-container">
                 <div className="current-user-container-header">Users</div>
                 <div className="current-all-users-container-div">
-                    { currentUsers.map((currentUser, index) => (
-                        <CurrentUser
-                            key={currentUsers.length - index - 1}
-                            name={currentUsers[currentUsers.length - index - 1]}
-                            connectionStatus={currentConnectionStatus[currentUsers.length - index - 1]}
-                            email={currentUserEmails[currentUsers.length - index - 1]}
-                            showFriendRequest={true}/>
-                    ))}
+                    { currentUsers.map((currentUser, index) => 
+                        {
+                            let email = currentUserEmails[currentUsers.length - index - 1];
+                            let showFriendRequest = !friends.has(email);
+                            console.log(email + " in friends: " + showFriendRequest);
+                            return (<CurrentUser
+                                key={currentUsers.length - index - 1}
+                                name={currentUsers[currentUsers.length - index - 1]}
+                                connectionStatus={currentConnectionStatus[currentUsers.length - index - 1]}
+                                email={email}
+                                showFriendRequest={showFriendRequest}/>)
+                        }
+                    )}
                 </div>
             </div>
         </div>
